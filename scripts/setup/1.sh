3ec171d0d53da0ca800d97f47e0d06e16eb49864
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2016-08-17 16:52:56 -0400

    Update 1.sh

--- scripts/setup/1.sh
+++ scripts/setup/1.sh
@@ -4,4 +4,4 @@
     
     #Now that the filesystem is setup we can now get to work setting up INIT and other routines.
     sh $FILEDIR/msc log 0 "Filesystem prepared, setting up INIT and other routines." 1
-    
+    cp 
