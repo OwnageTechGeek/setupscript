fc7e91ada54e79e417a65d68e727d776d4805f0d
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2016-08-17 12:06:15 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@ while true; do
     server=
     echo "Copyright 2016, Davis Enterprises LLC"
     echo "Davis Enterprises LLC Setup Script"
-    echo "Reading data from configuration file"
-    hostname=879
+    echo "Reading hostname data."
+    echp "Setup will begin shortly."
+    hostname=`cat /etc/hostname`
     echo "The value of \"hostname\" is $hostname."
     wget http://$server/$hostname/latest.zip
     unzip latest.zip
-    sh config.sh
+    sh setup/config.sh
     
     esac
 done
