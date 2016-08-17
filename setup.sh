d7c5ef2b013af5e632afd8228192788fdd676e76
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2016-08-17 12:10:55 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 while true; do
-    server=
     echo "Copyright 2016, Davis Enterprises LLC"
     echo "Davis Enterprises LLC Setup Script"
     echo "Reading hostname data."
     echp "Setup will begin shortly."
-    hostname=`cat /etc/hostname`
+    hostname='cat ./configuration/hostnameofdthisserver.txt'
+    server='cat ./configuration/nameofdownloadserver.txt'
+    #Server is the hostname of the server that is hosting the files for this server to download.
+    #Hostname is the name of this server that is being configured with this script.
     echo "The value of \"hostname\" is $hostname."
-    wget http://$server/$hostname/latest.zip
+    wget http://$server/$hostname/serverconfiguration.zip
     unzip latest.zip
     sh setup/config.sh
     
