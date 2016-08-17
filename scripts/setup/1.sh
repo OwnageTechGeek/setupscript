5893aba549af95ab007ffea3a46f45ef5661df02
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2016-08-17 16:37:04 -0400

    Update 1.sh

--- scripts/setup/1.sh
+++ scripts/setup/1.sh
@@ -1,4 +1,7 @@
 #/bin/bash
 
+    $FILEDIR='cat /srv/minecraft/configuration/filedir
+    
     #Now that the filesystem is setup we can now get to work setting up INIT and other routines.
+    sh $FILEDIR/msc log 0 "Filesystem prepared, setting up INIT and other routines." 1
     
